db368f20d2e0e717e6f348b1045eabd51f1d5622
yashb007 <bansaly37@gmail.com>
2020-05-22 14:26:11 +0530

    Updates

---
+++ precache-manifest.02be9e236f982b03996cdc03a90ae44b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "805341009224fafffdc2c9fa9fa2d7bb",
+    "url": "/covid-tracer/index.html"
+  },
+  {
+    "revision": "d8242fe36ddbaea91012",
+    "url": "/covid-tracer/static/css/main.f0aeacd9.chunk.css"
+  },
+  {
+    "revision": "40e3be2dc66cc0ede3d5",
+    "url": "/covid-tracer/static/js/2.8f973865.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-tracer/static/js/2.8f973865.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8242fe36ddbaea91012",
+    "url": "/covid-tracer/static/js/main.650c731b.chunk.js"
+  },
+  {
+    "revision": "00d2d4785117b1b36125",
+    "url": "/covid-tracer/static/js/runtime-main.e7d6a874.js"
+  }
+]);
